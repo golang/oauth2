701fa8dc864348ccbbf813f6d8928a1c50037c2a
Glenn Lewis <gmlewis@google.com>
2014-10-02 22:44:50 -0700

    Locally cache oauth tokens. This change is for both App Engine and Managed VMs so that these apps can scale without running into the app_identity_service quota limit due to calling appengine.AccessToken too frequently.  An added benefit of caching is that calls to Google APIs will be significantly sped up due to removing the round-trip calls to the api_identity_service.

--- google/appenginevm.go
+++ google/appenginevm.go
@@ -8,10 +8,23 @@ package google
 
 import (
 	"net/http"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/golang/oauth2"
 	"google.golang.org/appengine"
 )
+
+// mu protects multiple threads from attempting to fetch a token at the same time.
+var mu sync.Mutex
+
+// tokens implements a local cache of tokens to prevent hitting quota limits for appengine.AccessToken calls.
+var tokens map[string]*oauth2.Token
+
+func init() {
+	tokens = make(map[string]*oauth2.Token)
+}
 
 // AppEngineConfig represents a configuration for an
 // App Engine application's Google service account.
@@ -23,6 +36,9 @@ type AppEngineConfig struct {
 
 	context appengine.Context
 	scopes  []string
+
+	// key is the map key used to look up the cached tokens for this set of scopes.
+	key string
 }
 
 // NewAppEngineConfig creates a new AppEngineConfig for the
@@ -31,6 +47,7 @@ func NewAppEngineConfig(context appengine.Context, scopes ...string) *AppEngineC
 	return &AppEngineConfig{
 		context: context,
 		scopes:  scopes,
+		key:     strings.Join(scopes, "_"),
 	}
 }
 
@@ -41,15 +58,26 @@ func (c *AppEngineConfig) NewTransport() *oauth2.Transport {
 }
 
 // FetchToken fetches a new access token for the provided scopes.
+// Tokens are cached locally so that the app can scale without hitting quota limits
+// by calling appengine.AccessToken too frequently.
 func (c *AppEngineConfig) FetchToken(existing *oauth2.Token) (*oauth2.Token, error) {
+	mu.Lock()
+	defer mu.Unlock()
+	if t, ok := tokens[c.key]; ok && !t.Expiry.Before(time.Now()) {
+		return t, nil
+	}
+	delete(tokens, c.key)
+
 	token, expiry, err := appengine.AccessToken(c.context, c.scopes...)
 	if err != nil {
 		return nil, err
 	}
-	return &oauth2.Token{
+	t := &oauth2.Token{
 		AccessToken: token,
 		Expiry:      expiry,
-	}, nil
+	}
+	tokens[c.key] = t
+	return t, nil
 }
 
 func (c *AppEngineConfig) transport() http.RoundTripper {
