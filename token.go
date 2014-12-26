e5909d4679a1926c774c712b343f10b8298687a3
Jay Weisskopf <jay@jayschwa.net>
2014-12-26 16:20:45 -0600

    oauth2: remove duplicate package doc and import comments
    The comments already live in oauth2.go.
    
    Change-Id: I103113fbc8138b242cb6f79052fdc6b29bf6f031
    Reviewed-on: https://go-review.googlesource.com/2131
    Reviewed-by: Andrew Gerrand <adg@golang.org>

--- token.go
+++ token.go
@@ -2,10 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package oauth2 provides support for making
-// OAuth2 authorized and authenticated HTTP requests.
-// It can additionally grant authorization with Bearer JWT.
-package oauth2 // import "golang.org/x/oauth2"
+package oauth2
 
 import (
 	"net/http"
