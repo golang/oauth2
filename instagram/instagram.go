2d643292768610cdb5896827270dcd4a71da3572
a.lukinykh <a.lukinykh@xsolla.com>
2018-07-24 16:17:30 +0500

    instagram: add Instagram endpoints

--- instagram/instagram.go
+++ instagram/instagram.go
@@ -2,14 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package instagram provides constants for using OAuth2 to access instagram.com.
+// Package instagram provides constants for using OAuth2 to access Instagram.
 package instagram // import "golang.org/x/oauth2/instagram"
 
 import (
 	"golang.org/x/oauth2"
 )
 
-// Endpoint is Instagram OAuth 2.0 endpoint.
+// Endpoint is Instagram's OAuth 2.0 endpoint.
 var Endpoint = oauth2.Endpoint{
 	AuthURL:  "https://api.instagram.com/oauth/authorize",
 	TokenURL: "https://api.instagram.com/oauth/access_token",
