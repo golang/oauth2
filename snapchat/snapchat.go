c81b0b1451e4c8f109eea0224d50c67fc3b8073e
Josh Fyne <josh.fyne@essenceglobal.com>
2019-01-15 16:30:56 +0200

    Add new package with Snapchat's endpoint

---
+++ snapchat/snapchat.go
@@ -0,0 +1,16 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package snapchat provides constants for using OAuth2 to access Snapchat.
+package snapchat // import "golang.org/x/oauth2/stackoverflow"
+
+import (
+	"golang.org/x/oauth2"
+)
+
+// Endpoint is Snapchat's OAuth 2.0 endpoint.
+var Endpoint = oauth2.Endpoint{
+	AuthURL:  "https://accounts.snapchat.com/login/oauth2/authorize",
+	TokenURL: "https://accounts.snapchat.com/login/oauth2/access_token",
+}
