3ffadc1268828016a6b658a089f30c97a8670c1f
jonreiter@gmail.com <jonreiter@gmail.com>
2020-01-29 07:15:41 +0800

    cla signed and small fixes

--- clientcredentials/example_test.go
+++ clientcredentials/example_test.go
@@ -2,27 +2,25 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package oauth2_test
+package clientcredentials_test
 
 import (
 	"context"
 
-	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
 )
 
 func ExampleConfig() {
 	ctx := context.Background()
-	conf := &oauth2.Config{
+	conf := &clientcredentials.Config{
 		ClientID:     "YOUR_CLIENT_ID",
 		ClientSecret: "YOUR_CLIENT_SECRET",
 		Scopes:       []string{"SCOPE1", "SCOPE2"},
-		Endpoint: oauth2.Endpoint{
-			TokenURL: "https://provider.com/o/oauth2/token",
-		},
+		TokenURL:     "https://provider.com/o/oauth2/token",
 	}
 
 	// For the clientcredentials flow simply get a client from
 	// the Config directly.
-	client := conf.Client(ctx, tok)
+	client := conf.Client(ctx)
 	client.Get("...")
 }
