802bddef7cffcc54900b213014979f94f6cecaa9
Patrick Jones <ithuriel@google.com>
2020-10-06 01:18:45 -0700

    Run go fmt.

--- google/internal/oauth/clientAuthentication_test.go
+++ google/internal/oauth/clientAuthentication_test.go
@@ -8,39 +8,38 @@ import (
 	"testing"
 )
 
-
 var clientID = "rbrgnognrhongo3bi4gb9ghg9g"
 var clientSecret = "notsosecret"
-var valuesH = url.Values {
-	"audience": []string{"32555940559.apps.googleusercontent.com"},
-	"grant_type": []string{"urn:ietf:params:oauth:grant-type:token-exchange"},
+var valuesH = url.Values{
+	"audience":             []string{"32555940559.apps.googleusercontent.com"},
+	"grant_type":           []string{"urn:ietf:params:oauth:grant-type:token-exchange"},
 	"requested_token_type": []string{"urn:ietf:params:oauth:token-type:access_token"},
-	"subject_token_type": []string{"urn:ietf:params:oauth:token-type:jwt"},
-	"subject_token": []string{"eyJhbGciOiJSUzI1NiIsImtpZCI6IjJjNmZhNmY1OTUwYTdjZTQ2NWZjZjI0N2FhMGIwOTQ4MjhhYzk1MmMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzMjU1NTk0MDU1OS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjMyNTU1OTQwNTU5LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTEzMzE4NTQxMDA5MDU3Mzc4MzI4IiwiaGQiOiJnb29nbGUuY29tIiwiZW1haWwiOiJpdGh1cmllbEBnb29nbGUuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiI5OVJVYVFrRHJsVDFZOUV0SzdiYXJnIiwiaWF0IjoxNjAxNTgxMzQ5LCJleHAiOjE2MDE1ODQ5NDl9.SZ-4DyDcogDh_CDUKHqPCiT8AKLg4zLMpPhGQzmcmHQ6cJiV0WRVMf5Lq911qsvuekgxfQpIdKNXlD6yk3FqvC2rjBbuEztMF-OD_2B8CEIYFlMLGuTQimJlUQksLKM-3B2ITRDCxnyEdaZik0OVssiy1CBTsllS5MgTFqic7w8w0Cd6diqNkfPFZRWyRYsrRDRlHHbH5_TUnv2wnLVHBHlNvU4wU2yyjDIoqOvTRp8jtXdq7K31CDhXd47-hXsVFQn2ZgzuUEAkH2Q6NIXACcVyZOrjBcZiOQI9IRWz-g03LzbzPSecO7I8dDrhqUSqMrdNUz_f8Kr8JFhuVMfVug"},
-	"scope": []string{"https://www.googleapis.com/auth/devstorage.full_control"},
+	"subject_token_type":   []string{"urn:ietf:params:oauth:token-type:jwt"},
+	"subject_token":        []string{"eyJhbGciOiJSUzI1NiIsImtpZCI6IjJjNmZhNmY1OTUwYTdjZTQ2NWZjZjI0N2FhMGIwOTQ4MjhhYzk1MmMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzMjU1NTk0MDU1OS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjMyNTU1OTQwNTU5LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTEzMzE4NTQxMDA5MDU3Mzc4MzI4IiwiaGQiOiJnb29nbGUuY29tIiwiZW1haWwiOiJpdGh1cmllbEBnb29nbGUuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiI5OVJVYVFrRHJsVDFZOUV0SzdiYXJnIiwiaWF0IjoxNjAxNTgxMzQ5LCJleHAiOjE2MDE1ODQ5NDl9.SZ-4DyDcogDh_CDUKHqPCiT8AKLg4zLMpPhGQzmcmHQ6cJiV0WRVMf5Lq911qsvuekgxfQpIdKNXlD6yk3FqvC2rjBbuEztMF-OD_2B8CEIYFlMLGuTQimJlUQksLKM-3B2ITRDCxnyEdaZik0OVssiy1CBTsllS5MgTFqic7w8w0Cd6diqNkfPFZRWyRYsrRDRlHHbH5_TUnv2wnLVHBHlNvU4wU2yyjDIoqOvTRp8jtXdq7K31CDhXd47-hXsVFQn2ZgzuUEAkH2Q6NIXACcVyZOrjBcZiOQI9IRWz-g03LzbzPSecO7I8dDrhqUSqMrdNUz_f8Kr8JFhuVMfVug"},
+	"scope":                []string{"https://www.googleapis.com/auth/devstorage.full_control"},
 }
 
-var headerH = http.Header {
+var headerH = http.Header{
 	"Content-Type": []string{"application/x-www-form-urlencoded"},
 }
 
-var valuesP = url.Values {
-	"audience": []string{"32555940559.apps.googleusercontent.com"},
-	"grant_type": []string{"urn:ietf:params:oauth:grant-type:token-exchange"},
+var valuesP = url.Values{
+	"audience":             []string{"32555940559.apps.googleusercontent.com"},
+	"grant_type":           []string{"urn:ietf:params:oauth:grant-type:token-exchange"},
 	"requested_token_type": []string{"urn:ietf:params:oauth:token-type:access_token"},
-	"subject_token_type": []string{"urn:ietf:params:oauth:token-type:jwt"},
-	"subject_token": []string{"eyJhbGciOiJSUzI1NiIsImtpZCI6IjJjNmZhNmY1OTUwYTdjZTQ2NWZjZjI0N2FhMGIwOTQ4MjhhYzk1MmMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzMjU1NTk0MDU1OS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjMyNTU1OTQwNTU5LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTEzMzE4NTQxMDA5MDU3Mzc4MzI4IiwiaGQiOiJnb29nbGUuY29tIiwiZW1haWwiOiJpdGh1cmllbEBnb29nbGUuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiI5OVJVYVFrRHJsVDFZOUV0SzdiYXJnIiwiaWF0IjoxNjAxNTgxMzQ5LCJleHAiOjE2MDE1ODQ5NDl9.SZ-4DyDcogDh_CDUKHqPCiT8AKLg4zLMpPhGQzmcmHQ6cJiV0WRVMf5Lq911qsvuekgxfQpIdKNXlD6yk3FqvC2rjBbuEztMF-OD_2B8CEIYFlMLGuTQimJlUQksLKM-3B2ITRDCxnyEdaZik0OVssiy1CBTsllS5MgTFqic7w8w0Cd6diqNkfPFZRWyRYsrRDRlHHbH5_TUnv2wnLVHBHlNvU4wU2yyjDIoqOvTRp8jtXdq7K31CDhXd47-hXsVFQn2ZgzuUEAkH2Q6NIXACcVyZOrjBcZiOQI9IRWz-g03LzbzPSecO7I8dDrhqUSqMrdNUz_f8Kr8JFhuVMfVug"},
-	"scope": []string{"https://www.googleapis.com/auth/devstorage.full_control"},
+	"subject_token_type":   []string{"urn:ietf:params:oauth:token-type:jwt"},
+	"subject_token":        []string{"eyJhbGciOiJSUzI1NiIsImtpZCI6IjJjNmZhNmY1OTUwYTdjZTQ2NWZjZjI0N2FhMGIwOTQ4MjhhYzk1MmMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzMjU1NTk0MDU1OS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjMyNTU1OTQwNTU5LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTEzMzE4NTQxMDA5MDU3Mzc4MzI4IiwiaGQiOiJnb29nbGUuY29tIiwiZW1haWwiOiJpdGh1cmllbEBnb29nbGUuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiI5OVJVYVFrRHJsVDFZOUV0SzdiYXJnIiwiaWF0IjoxNjAxNTgxMzQ5LCJleHAiOjE2MDE1ODQ5NDl9.SZ-4DyDcogDh_CDUKHqPCiT8AKLg4zLMpPhGQzmcmHQ6cJiV0WRVMf5Lq911qsvuekgxfQpIdKNXlD6yk3FqvC2rjBbuEztMF-OD_2B8CEIYFlMLGuTQimJlUQksLKM-3B2ITRDCxnyEdaZik0OVssiy1CBTsllS5MgTFqic7w8w0Cd6diqNkfPFZRWyRYsrRDRlHHbH5_TUnv2wnLVHBHlNvU4wU2yyjDIoqOvTRp8jtXdq7K31CDhXd47-hXsVFQn2ZgzuUEAkH2Q6NIXACcVyZOrjBcZiOQI9IRWz-g03LzbzPSecO7I8dDrhqUSqMrdNUz_f8Kr8JFhuVMfVug"},
+	"scope":                []string{"https://www.googleapis.com/auth/devstorage.full_control"},
 }
 
-var headerP = http.Header {
+var headerP = http.Header{
 	"Content-Type": []string{"application/x-www-form-urlencoded"},
 }
 
 func TestClientAuthentication_InjectHeaderAuthentication(t *testing.T) {
 	headerAuthentication := ClientAuthentication{
-		AuthStyle: oauth2.AuthStyleInHeader,
-		ClientID: clientID,
+		AuthStyle:    oauth2.AuthStyleInHeader,
+		ClientID:     clientID,
 		ClientSecret: clientSecret,
 	}
 	headerAuthentication.InjectAuthentication(&valuesH, &headerH)
@@ -63,15 +62,15 @@ func TestClientAuthentication_InjectHeaderAuthentication(t *testing.T) {
 	if got, want := valuesH["scope"], []string{"https://www.googleapis.com/auth/devstorage.full_control"}; !reflect.DeepEqual(got, want) {
 		t.Errorf("scope = %q, want %q", got, want)
 	}
-	if got, want := headerH["Authorization"],[]string{"Basic cmJyZ25vZ25yaG9uZ28zYmk0Z2I5Z2hnOWc6bm90c29zZWNyZXQ="}; !reflect.DeepEqual(got, want) {
+	if got, want := headerH["Authorization"], []string{"Basic cmJyZ25vZ25yaG9uZ28zYmk0Z2I5Z2hnOWc6bm90c29zZWNyZXQ="}; !reflect.DeepEqual(got, want) {
 		t.Errorf("Authorization in header = %q, want %q", got, want)
 	}
 }
 
 func TestClientAuthentication_ParamsAuthentication(t *testing.T) {
 	paramsAuthentication := ClientAuthentication{
-		AuthStyle: oauth2.AuthStyleInParams,
-		ClientID: clientID,
+		AuthStyle:    oauth2.AuthStyleInParams,
+		ClientID:     clientID,
 		ClientSecret: clientSecret,
 	}
 	paramsAuthentication.InjectAuthentication(&valuesP, &headerP)
