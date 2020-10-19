f88c24c4528dcb8cc565f9b863a8fa43e21a8312
Patrick Jones <ithuriel@google.com>
2020-10-19 13:29:39 -0700

    Updated comment formatting.

--- google/internal/externalaccount/oauth/clientauth.go
+++ google/internal/externalaccount/oauth/clientauth.go
@@ -9,7 +9,7 @@ import (
 
 // ClientAuthentication represents an OAuth client ID and secret and the mechanism for passing these credentials as stated in rfc6749#2.3.1.
 type ClientAuthentication struct {
-	//Can be either basic or request-body
+	//AuthStyle can be either basic or request-body
 	AuthStyle    oauth2.AuthStyle
 	ClientID     string
 	ClientSecret string
