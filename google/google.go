14806e6b37a019cbff58e2088ee99191e89b4f7e
Guillaume Blaquiere <guillaume.blaquiere@gmail.com>
2021-10-28 19:46:04 +0200

    fix: wrong test case.

--- google/google.go
+++ google/google.go
@@ -186,7 +186,7 @@ func (f *credentialsFile) tokenSource(ctx context.Context, params CredentialsPar
 		}
 		return cfg.TokenSource(ctx)
 	case impersonatedServiceAccount:
-		if f.ServiceAccountImpersonationURL != "" || f.SourceCredentials == nil {
+		if f.ServiceAccountImpersonationURL == "" || f.SourceCredentials == nil {
 			return nil, errors.New("missing 'source_credentials' field or 'service_account_impersonation_url' in credentials")
 		}
 
