67a55f1070a3ab1642e19ea000f89aae7863b267
David Shi <85764004+davids-codex@users.noreply.github.com>
2022-03-22 15:10:40 -0700

    Update oauth2.go

--- oauth2.go
+++ oauth2.go
@@ -17,7 +17,7 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/davids-codex/oauth2/internal"
+	"golang.org/x/oauth2/internal"
 )
 
 // NoContext is the default context you should supply if not using
