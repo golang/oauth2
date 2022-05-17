622c5d57e401754bcdaf99beee1fe0c1136fe3d9
Shapor Naghibzadeh <shapor@gmail.com>
2022-05-17 10:13:16 -0700

    google/google: set JWT Audience in JWTConfigFromJSON()
    Add support to set JWT Audience in JWTConfigFromJSON() to allow setting
    the audience field from the JSON config, rather than only allowing it
    the default value of the token_uri.
    
    Previous change 272766 (approved but abandoned).
    
    Change-Id: I14d46f3628df0a04801949bf99520b210e778f99
    Reviewed-on: https://go-review.googlesource.com/c/oauth2/+/406836
    Reviewed-by: Cody Oss <codyoss@google.com>
    Run-TryBot: Cody Oss <codyoss@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@google.com>

--- google/google_test.go
+++ google/google_test.go
@@ -37,7 +37,8 @@ var jwtJSONKey = []byte(`{
   "client_email": "gopher@developer.gserviceaccount.com",
   "client_id": "gopher.apps.googleusercontent.com",
   "token_uri": "https://accounts.google.com/o/gophers/token",
-  "type": "service_account"
+  "type": "service_account",
+  "audience": "https://testservice.googleapis.com/"
 }`)
 
 var jwtJSONKeyNoTokenURL = []byte(`{
@@ -45,6 +46,15 @@ var jwtJSONKeyNoTokenURL = []byte(`{
   "private_key": "super secret key",
   "client_email": "gopher@developer.gserviceaccount.com",
   "client_id": "gopher.apps.googleusercontent.com",
+  "type": "service_account"
+}`)
+
+var jwtJSONKeyNoAudience = []byte(`{
+  "private_key_id": "268f54e43a1af97cfc71731688434f45aca15c8b",
+  "private_key": "super secret key",
+  "client_email": "gopher@developer.gserviceaccount.com",
+  "client_id": "gopher.apps.googleusercontent.com",
+  "token_uri": "https://accounts.google.com/o/gophers/token",
   "type": "service_account"
 }`)
 
@@ -103,6 +113,9 @@ func TestJWTConfigFromJSON(t *testing.T) {
 	if got, want := conf.TokenURL, "https://accounts.google.com/o/gophers/token"; got != want {
 		t.Errorf("TokenURL = %q; want %q", got, want)
 	}
+	if got, want := conf.Audience, "https://testservice.googleapis.com/"; got != want {
+		t.Errorf("Audience = %q; want %q", got, want)
+	}
 }
 
 func TestJWTConfigFromJSONNoTokenURL(t *testing.T) {
@@ -112,5 +125,15 @@ func TestJWTConfigFromJSONNoTokenURL(t *testing.T) {
 	}
 	if got, want := conf.TokenURL, "https://oauth2.googleapis.com/token"; got != want {
 		t.Errorf("TokenURL = %q; want %q", got, want)
+	}
+}
+
+func TestJWTConfigFromJSONNoAudience(t *testing.T) {
+	conf, err := JWTConfigFromJSON(jwtJSONKeyNoAudience, "scope1", "scope2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := conf.Audience, ""; got != want {
+		t.Errorf("Audience = %q; want %q", got, want)
 	}
 }
