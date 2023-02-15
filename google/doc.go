15d7759884817d0f835768bdb5e5b3fa86e8fdbf
aeitzman <eitzman@google.com>
2023-02-14 16:24:05 -0800

    Moved warning message in doc

--- google/doc.go
+++ google/doc.go
@@ -57,6 +57,11 @@
 // executable-sourced credentials), please check out:
 // https://cloud.google.com/iam/docs/using-workload-identity-federation#oidc
 //
+// Note that this library does not perform any validation on the token_url, token_info_url,
+// or service_account_impersonation_url fields of the credential configuration.
+// It is not recommended to use a credential configuration that you did not generate with
+// the gcloud CLI unless you verify that the URL fields point to a googleapis.com domain.
+//
 // # Credentials
 //
 // The Credentials type represents Google credentials, including Application Default
@@ -82,8 +87,4 @@
 // JWTConfigFromJSON, but the Credentials may contain additional information
 // that is useful is some circumstances.
 //
-// Note that this library does not perform any validation on the token_url, token_info_url,
-// or service_account_impersonation_url fields of the credential configuration.
-// It is not recommended to use a credential configuration that you did not generate with
-// the gcloud CLI unless you verify that the URL fields point to a googleapis.com domain.
 package google // import "golang.org/x/oauth2/google"
