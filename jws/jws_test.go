ce56909505b351a755ad0bc294c5ee01ed0ea050
Julien Cretel <jub0bsinthecloud@gmail.com>
2025-03-11 18:26:19 +0000

    jws: improve fix for CVE-2025-22868
    The fix for CVE-2025-22868 relies on strings.Count, which isn't ideal
    because it precludes failing fast when the token contains an unexpected
    number of periods. Moreover, Verify still allocates more than necessary.
    
    Eschew strings.Count in favor of strings.Cut. Some benchmark results:
    
    goos: darwin
    goarch: amd64
    pkg: golang.org/x/oauth2/jws
    cpu: Intel(R) Core(TM) i7-6700HQ CPU @ 2.60GHz
                                  │      old       │                 new                 │
                                  │     sec/op     │   sec/op     vs base                │
    Verify/full_of_periods-8        24862.50n ± 1%   57.87n ± 0%  -99.77% (p=0.000 n=20)
    Verify/two_trailing_periods-8      3.485m ± 1%   3.445m ± 1%   -1.13% (p=0.003 n=20)
    geomean                            294.3µ        14.12µ       -95.20%
    
                                  │     old      │                  new                   │
                                  │     B/op     │     B/op      vs base                  │
    Verify/full_of_periods-8          16.00 ± 0%     16.00 ± 0%        ~ (p=1.000 n=20) ¹
    Verify/two_trailing_periods-8   2.001Mi ± 0%   1.001Mi ± 0%  -49.98% (p=0.000 n=20)
    geomean                         5.658Ki        4.002Ki       -29.27%
    ¹ all samples are equal
    
                                  │     old     │                 new                  │
                                  │  allocs/op  │ allocs/op   vs base                  │
    Verify/full_of_periods-8         1.000 ± 0%   1.000 ± 0%        ~ (p=1.000 n=20) ¹
    Verify/two_trailing_periods-8   12.000 ± 0%   9.000 ± 0%  -25.00% (p=0.000 n=20)
    geomean                          3.464        3.000       -13.40%
    ¹ all samples are equal
    
    Also, remove all remaining calls to strings.Split.
    
    Updates golang/go#71490
    
    Change-Id: Icac3c7a81562161ab6533d892ba19247d6d5b943
    GitHub-Last-Rev: 3a82900f747798f5f36065126385880277c0fce7
    GitHub-Pull-Request: golang/oauth2#774
    Reviewed-on: https://go-review.googlesource.com/c/oauth2/+/655455
    Commit-Queue: Neal Patel <nealpatel@google.com>
    Reviewed-by: Roland Shoemaker <roland@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Neal Patel <nealpatel@google.com>
    Auto-Submit: Neal Patel <nealpatel@google.com>

--- jws/jws_test.go
+++ jws/jws_test.go
@@ -7,6 +7,8 @@ package jws
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"net/http"
+	"strings"
 	"testing"
 )
 
@@ -39,8 +41,57 @@ func TestSignAndVerify(t *testing.T) {
 }
 
 func TestVerifyFailsOnMalformedClaim(t *testing.T) {
-	err := Verify("abc.def", nil)
-	if err == nil {
-		t.Error("got no errors; want improperly formed JWT not to be verified")
+	cases := []struct {
+		desc  string
+		token string
+	}{
+		{
+			desc:  "no periods",
+			token: "aa",
+		}, {
+			desc:  "only one period",
+			token: "a.a",
+		}, {
+			desc:  "more than two periods",
+			token: "a.a.a.a",
+		},
+	}
+	for _, tc := range cases {
+		f := func(t *testing.T) {
+			err := Verify(tc.token, nil)
+			if err == nil {
+				t.Error("got no errors; want improperly formed JWT not to be verified")
+			}
+		}
+		t.Run(tc.desc, f)
+	}
+}
+
+func BenchmarkVerify(b *testing.B) {
+	cases := []struct {
+		desc  string
+		token string
+	}{
+		{
+			desc:  "full of periods",
+			token: strings.Repeat(".", http.DefaultMaxHeaderBytes),
+		}, {
+			desc:  "two trailing periods",
+			token: strings.Repeat("a", http.DefaultMaxHeaderBytes-2) + "..",
+		},
+	}
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		b.Fatal(err)
+	}
+	for _, bc := range cases {
+		f := func(b *testing.B) {
+			b.ReportAllocs()
+			b.ResetTimer()
+			for range b.N {
+				Verify(bc.token, &privateKey.PublicKey)
+			}
+		}
+		b.Run(bc.desc, f)
 	}
 }
