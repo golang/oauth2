fd15e0fe894866ebff17ca3503d3706a967b061a
andig <cpuidle@gmail.com>
2025-10-18 11:46:11 +0000

    x/oauth2: populate RetrieveError from DeviceAuth
    Endpoints may return errors when attempting to request device
    authorization. Currently, these error codes are ignored and an
    otherwise empty RetrieveError returned. This change populates
    the RetrieveError similar to the oauth2 token exchange.
    
    Fixes golang/go#75759
    
    Change-Id: Ic00fecce290d3d3b4a40697b54ce74cc8cacab4d
    GitHub-Last-Rev: 32c6ab5b8c0f8c2624cba0d536224a1fefcf0665
    GitHub-Pull-Request: golang/oauth2#794
    Reviewed-on: https://go-review.googlesource.com/c/oauth2/+/709215
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: David Chase <drchase@google.com>
    Reviewed-by: Sean Liao <sean@liao.dev>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>

--- deviceauth_test.go
+++ deviceauth_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"strings"
 	"testing"
 	"time"
@@ -100,4 +102,53 @@ func ExampleConfig_DeviceAuth() {
 		panic(err)
 	}
 	fmt.Println(token)
+}
+
+func TestDeviceAuthTokenRetrieveError(t *testing.T) {
+	runner := func(responseFun func(w http.ResponseWriter)) func(t *testing.T) {
+		return func(t *testing.T) {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r.URL.String() != "/device" {
+					t.Errorf("Unexpected device auth request URL, %v is found.", r.URL)
+				}
+				responseFun(w)
+			}))
+			defer ts.Close()
+			conf := newConf(ts.URL)
+			_, err := conf.DeviceAuth(context.Background())
+			if err == nil {
+				t.Fatalf("got no error, expected one")
+			}
+			re, ok := err.(*RetrieveError)
+			if !ok {
+				t.Fatalf("got %T error, expected *RetrieveError; error was: %v", err, err)
+			}
+			expected := `oauth2: "invalid_grant" "sometext"`
+			if errStr := err.Error(); errStr != expected {
+				t.Fatalf("got %#v, expected %#v", errStr, expected)
+			}
+			expected = "invalid_grant"
+			if re.ErrorCode != expected {
+				t.Fatalf("got %#v, expected %#v", re.ErrorCode, expected)
+			}
+			expected = "sometext"
+			if re.ErrorDescription != expected {
+				t.Fatalf("got %#v, expected %#v", re.ErrorDescription, expected)
+			}
+		}
+	}
+
+	t.Run("UrlEncoding", runner(func(w http.ResponseWriter) {
+		w.Header().Set("Content-type", "application/x-www-form-urlencoded")
+		// "The authorization server responds with an HTTP 400 (Bad Request)" https://www.rfc-editor.org/rfc/rfc6749#section-5.2
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`error=invalid_grant&error_description=sometext`))
+	}))
+
+	t.Run("JSON", runner(func(w http.ResponseWriter) {
+		w.Header().Set("Content-type", "application/json")
+		// "The authorization server responds with an HTTP 400 (Bad Request)" https://www.rfc-editor.org/rfc/rfc6749#section-5.2
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error": "invalid_grant", "error_description": "sometext"}`))
+	}))
 }
